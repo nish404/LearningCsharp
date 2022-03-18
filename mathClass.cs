80bac107a3c2cd8f9b51c312a861ae2cf59f8e4c
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 08:42:22 -0400

    Create mathClass.cs

---
+++ mathClass.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3.99;
+            double y = 5;
+
+            //double a = Math.Pow(x, 2);
+            //double b = Math.Sqrt(x);
+            //double c = Math.Abs(x),
+            //double d = Math.Round(x);
+            //double e = Math.Ceiling(x);
+            //double f = Math.Floor(x);
+            //double g = Math.Max(x, y);
+            double h = Math.Min(x, y);
+
+            Console.WriteLine(h);
+
+            Console.ReadKey();
+        }
+    }
+}
