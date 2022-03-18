b7bee0f20785509848aece6962a476306e4dac55
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 08:38:18 -0400

    Create constants.cs

---
+++ constants.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constants = imutable values which are known at compile time
+            //             and do not change for the life of the program
+
+            const double pi = 3.1459;
+
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
+        }
+    }
+}
