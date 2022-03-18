e4a2cec5a00bd6a6f728de984ae5bddf39dd0e4e
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 08:41:00 -0400

    Create conditionalOperator.cs

---
+++ conditionalOperator.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // conditional operator = used in conditional assignment if a condition is true/false
+            // (condition) ? x :y
+
+            double temperature = 20;
+            String message;
+
+            message = (temperature >= 15) ? "it's warm outside!" : "it's cold outside";
+
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
+using System;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // conditional operator = used in conditional assignment if a condition is true/false
+            // (condition) ? x :y
+
+            double temperature = 20;
+            String message;
+
+            message = (temperature >= 15) ? "it's warm outside!" : "it's cold outside";
+
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
