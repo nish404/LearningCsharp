5dc27d5ed34ab7491aa4f322f427264e3612904c
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 08:39:25 -0400

    Create userInput.cs

---
+++ userInput.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("What's your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello "+name);
+            Console.WriteLine("You are " + age + " years old");
+
+            Console.ReadKey();
+        }
+    }
+}
