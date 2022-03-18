acbaad1ca904c89f9da7b4c8adb10c2873fccaa7
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 09:10:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+    public class Practice
+    {
+        static void Main(String[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            TwoSum(nums, target);
+        }
+
+        public static int TwoSum(int[] nums, int target)
+        {
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
