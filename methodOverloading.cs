38cb53e1a6de4ab9592efbc88e97df92f96772a1
nish159 <77575013+nish159@users.noreply.github.com>
2022-03-18 08:57:09 -0400

    Create methodOverloading.cs

---
+++ methodOverloading.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method overloading = methods share same name, but different parameters
+            //                      name + parameters = signature 
+            //                      methods must have a unique signature 
+
+            double total;
+
+            total = Multiply(2, 3, 4);
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        static double Multiply(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+    }
+}
