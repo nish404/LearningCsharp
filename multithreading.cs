7f4beea9f33c76d2e5a78477a41947413955b8b9
nish159 <77575013+nish159@users.noreply.github.com>
2022-07-12 10:01:12 -0400

    Update multithreading.cs

--- multithreading.cs
+++ multithreading.cs
@@ -1,46 +1,41 @@
 using System;
 using System.Threading;
 
-namespace learningCsharp
+/* thread = an execution path of a program we can use to perform 
+ *          different tasks of our program at the same time 
+ *          current thread running is a "main" thread
+ *          using System.Threading
+ *          => = lambda function
+ */
+
+Thread mainThread = Thread.CurrentThread;
+mainThread.Name = "Main Thread";
+
+Console.WriteLine(mainThread.Name);
+
+Thread thread1 = new Thread(() => CountDown("Timer #1"));
+Thread thread2 = new Thread(() => CountUp("Timer #2"));
+thread1.Start();
+thread2.Start();
+
+Console.WriteLine(mainThread.Name + " is complete");
+
+Console.ReadKey();
+
+static void CountDown(String name)
 {
-    class Program
+    for (int i = 10; i >= 0; i--)
     {
-        static void Main(string[] args)
-        {
-            // thread = an execution path of a program
-            //          we can use multiple threads to perform
-            //          different tasks of our program at the same time
-            //          current thread running is "main" thread
-            //          using System.Threading;
+        Console.WriteLine("Timer #1 : " + i +  " seconds");
+        Thread.Sleep(1000);
+    }
+}
 
-            Thread mainThread = Thread.CurrentThread;
-            mainThread.Name = "Main Thread";
-            //Console.WriteLine(mainThread.Name);
-
-            Thread thread1 = new Thread(() => CountDown("Timer #1"));
-            Thread thread2 = new Thread(() => CountUp("Timer #2"));
-            thread1.Start();
-            thread2.Start();
-
-            Console.WriteLine(mainThread.Name + " is complete");
-
-            Console.ReadKey();
-        }
-        public static void CountDown(String name)
-        {
-            for (int i = 10; i >= 0; i--)
-            {
-                Console.WriteLine("Timer #1 : " + " seconds");
-                Thread.Sleep(1000);
-            }
-        }
-        public static void CountUp(String name)
-        {
-            for (int i = 0; i <= 10; i++)
-            {
-                Console.WriteLine("Timer #1 : " + " seconds");
-                Thread.Sleep(1000);
-            }
-        }
+static void CountUp(String name)
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine("Timer #2 : " + i + " seconds");
+        Thread.Sleep(1000);
     }
 }
